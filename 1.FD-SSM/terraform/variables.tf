0435dc8b56ad786b0d9bec6c3fc1ada4239f2d39
xorverbin <145239266+xorverbin@users.noreply.github.com>
2023-12-30 13:13:33 +0900

    Add files via upload
    upload terraform

---
+++ 1.FD-SSM/terraform/variables.tf
@@ -0,0 +1,20 @@
+
+variable "profile" {
+
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "crid" {
+
+}
+
+variable "stack-name" {
+  default = "CloudRudolf"
+}
+
+variable "scenario-name" {
+  default = "FD-SSM"
+}
